d4a5bb5edc0b9b499bc43357b5e1ed5930032cf6
mailsonfp <mailsonfp.dev@gmail.com>
2024-07-09 22:09:08 -0300

    adiciona exercicio resolvido

--- teste2.py
+++ teste2.py
@@ -14,3 +14,21 @@ if not (b and c):
     print('É isso ai parça')
 else:
     print('deu ruim!')
+
+m1 = float(input('Digite a nota M1: '))
+m2 = float(input('Digite a nota M2: '))
+m3 = float(input('Digite a nota M3: '))
+
+media = (m1 + m2 + m3) / 3
+print(media)
+
+if 0.0 <= media <= 4.0:
+    print('Aluno reprovado')
+elif 4.1 <= media <= 6.0:
+    exame = float(input('Digite a nota do exame'))
+    if exame >= 6.0:
+        print('Aprovado no exame')
+    else:
+        print('Reprovado no exame')
+else:
+    print('Aluno aprovado')
