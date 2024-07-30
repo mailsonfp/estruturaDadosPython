d68a8876dfe929b8703b73efe453954eec3e6430
mailsonfp <mailsonfp.dev@gmail.com>
2024-07-30 20:29:21 -0300

    adda 29-07-24

--- busca_binaria_vetor_ordenado.py
+++ busca_binaria_vetor_ordenado.py
@@ -18,9 +18,31 @@ def search(nums, target):
     return index
 
 
+def solution_sum(array):
+    mid = len(array) // 2
+    print(f'len: {len(array) // 2}')
+
+    sum_one = 1
+    sum_two = 1
+    # for i in range(mid+1):
+    #     print(f'array[i]: {array[i]}')
+    #     sum_one *= array[i]
+    #
+    #     print(f'array[mid+i]: {array[mid+i]}')
+    #     sum_two *= array[mid+i]
+    line = [0] * len(array)
+    for i in range(array):
+        for j in range(len(array), -1, -1):
+            line.append(array[i] * array[j] * array[j-1])
+
+    print(line)
+
+    return 0
+
+
 if __name__ == '__main__':
-    nums = [-1, 0, 3, 5, 9, 12]
-    target = 2
-    print(search(nums, target))
-    target = 9
-    print(search(nums, target))
+    nums = [-10, 1, -10, 3, 2]
+    print(solution_sum(nums))
+
+    nums = [1, 2, 3, 4, 5]
+    print(solution_sum(nums))
